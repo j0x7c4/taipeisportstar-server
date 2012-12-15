0901cb7efe9ab43d466113d8eed9d10045a1b518
j0x7c4 <china.eli@gmail.com>
2012-12-16 01:05:47 +0800

    add event

---
+++ event.py
@@ -0,0 +1,23 @@
+import weather
+import json
+import webapp2
+import datetime 
+from google.appengine.ext import db
+
+class Event ( db.model ):
+	event_id = db.StringProperty()
+	#create_date = db.DateProperty()
+	#expired_date = db.DateProperty()
+	#sport_id = db.StringProperty()
+	#stadium_id = db.StringProperty()
+	#owner_id = db.StringProperty()
+
+class CreateEventPage(webapp2.RequestHandler):
+	def get(self, event_id_):
+		event = Event( event_id = str(event_id_) )
+		text = event.put()
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write(text)
+
+app = webapp2.WSGIApplication([
+	('/api/event/create/(\d+)', CreateEventPage)],debug=True)
